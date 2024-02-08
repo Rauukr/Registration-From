f58a228ae3f1a44b7bf93de3da2287a3fb489b58
Rauukr <rauukr123@gmail.com>
2024-02-09 00:14:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+import  Express  from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import urlencoded from "body-parser";
+import path from "path"
+const __dirname = path.resolve();
+
+const app=Express();
+dotenv.config();
+
+const Port = process.env.Port || 3000;
+mongoose.connect("mongodb+srv://raushan:rau1234@cluster6.eqxhuls.mongodb.net/registrationFomrDb",{
+useNewUrlParser:true,
+useUnifiedTopology:true
+});
+
+const registerSchema=new mongoose.Schema({
+    name:String,
+    email:String,
+    password:String
+})
+const Regitser=mongoose.model("Regitser" , registerSchema);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+
+app.get("/" , (req ,res)=>{
+    res.sendFile(__dirname  +"/pages/index.html")
+})
+app.post("/register" , async (req, res)=>{
+    try {
+        const {email , name , password}=req.body;
+        const exitingUser=await Regitser.findOne({email:email});
+        if(!exitingUser){
+            const registrationData=new Regitser({
+                name,
+                email,
+                password
+            });
+             await registrationData.save();
+             res.redirect("/success");
+        }else{
+            console.log("user already exit");
+            res.redirect("/error");
+        }
+    } catch (error) {
+        console.log(error);
+        res.redirect("error");
+    }
+})
+
+app.get("/success" , (req ,res)=>{
+    res.sendFile(__dirname + "/pages/success.html")
+})
+app.get("/error" , (req ,res)=>{
+    res.sendFile(__dirname + "/pages/error.html");
+})
+
+app.listen(Port ,()=>{
+    console.log(`server is running on port ${Port}`);
+})
